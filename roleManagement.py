d6d91b3ff85a45f8d5d498f2a98e9d8a190e3a32
hoopandango <76955186+hoopandango@users.noreply.github.com>
2021-01-07 20:02:39 +0530

    Update roleManagement.py

--- roleManagement.py
+++ roleManagement.py
@@ -1,63 +1,92 @@
 import random
 import csv
 
-players = []
-roles = []    #Roles in the game
-#rolelist = ["Jailor","TI","TI","TP","TP","TK","TS","RT","RT","RT","Alphawolf","Wolf Apprentice","WW","WW","N","NK"]  #Default Rolelist
-allRolelists = []
-rolelist = []
-allroles = []  #list of all roles
-playercount = 15 #default player count
+players = []  # List of all players
+roles = []  # List of roles in current game
+allRolelists = {} # Collection of all rolelists
+rolelist = [] # Rolelist used in current game
+allRoles = [] # List of all roles [each role is a dictionary]
+playercount = 0 # Number of players
 
-def tobool(s):
+def clean():
+  global roles
+  global rolelist
+  global allRoles
+  roles.clear()
+  rolelist.clear()
+  allRoles.clear()
+  setAllRoles()
+  setAllRolelists()
+  setRolelist(playercount)
+
+def tobool(s): 
   return (s=='True')
 
-def setPlayercount(n):
-  playercount = n
-
-def getRolechoices(faction, *args):
-  if(len(args)>0):
-    return [t for t in allroles if t["roletype"] == args[0] and t["faction"] == faction]
-  else:
-    return [t for t in allroles if t["faction"] == faction]
-
-def initializeRolelist(playercount):
+def setAllRoles():
+  with open("RolesData.csv", 'r') as file:
+    global allRoles
+    reader = csv.DictReader(file)
+    for row in reader:
+      allRoles.append(row)
+    allRoles[-1]["unique"] = tobool(allRoles[-1]["unique"]) 
+    
+def setAllRolelists():
+  global allRolelists
   with open("RolelistData.csv", 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
-      allRolelists[int(row["count"])] = row["roles"].split(";")
+      allRolelists[row["count"]] = row["roles"].split(";")
+
+def setRolelist(playercount):
     if(playercount >= 8 and playercount <= 16):
-      rolelist = allRolelists[playercount]
+      global rolelist 
+      rolelist = allRolelists[str(playercount)]
 
-def initializeAllroles():
-  with open("RolesData.csv", 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-      allroles.append(row)
-    allroles[-1]["unique"] = tobool(allroles[-1]["unique"]) 
+def setPlayercount(num):  
+  global playercount 
+  playercount = num
 
-def assignRoles(rolelist): #assigns roles to roles[]
-  rolechoices= []  #roles that can be assigned in a slot
-  initializeAllroles()
-  
+def addPlayer(playerlist):
+  global players
+  playerarray = playerlist.split(',')
+  if(len(players)+len(playerarray) <= playercount):
+    players += playerarray
+
+def setDefaultPlayers():
+  global players 
+  players = string.ascii_uppercase[0:playercount]
+
+def setRoles(rolelist): #sets roles to roles[]
+  rolechoices= []  #roles that can be set in a slot
+  global roles
+  global allRoles
+  #if(playercount <= 11):
+  #    allRoles.remove([t for t in allRoles if t["name"] =="Jailor"])
+  #    allRoles.remove([t for t in allRoles if t["name"] =="Wolf Apprentice"])
   for e in rolelist:
     if e == "Jailor" or e == "Alphawolf" or e == "Wolf Apprentice":
-      rolechoices = [t for t in allroles if t["name"] == e] 
+      rolechoices = [t for t in allRoles if t["name"] == e] 
     elif e == "TI":
-      rolechoices = getRolechoices("T","I") 
+      rolechoices = findRolechoices("T","I") 
     elif e == "TP":
-      rolechoices = getRolechoices("T","P")   
+      rolechoices = findRolechoices("T","P")   
     elif e == "TK":
-      rolechoices = getRolechoices("T","K") 
+      rolechoices = findRolechoices("T","K") 
     elif e == "TS":
-      rolechoices = getRolechoices("T","S")   
+      rolechoices = findRolechoices("T","S")   
     else:
-      rolechoices = getRolechoices(e[-1]) 
+      rolechoices = findRolechoices(e[-1]) 
     chosenrole = random.choice(rolechoices) 
     roles.append(chosenrole["name"]) 
     if(chosenrole["unique"]):
-      allroles.remove(chosenrole) 
+      allRoles.remove(chosenrole) 
   return roles
+
+def findRolechoices(faction, *args):
+  if(len(args)>0):
+    return [t for t in allRoles if t["roletype"] == args[0] and t["faction"] == faction]
+  else:
+    return [t for t in allRoles if t["faction"] == faction]
 
 def playersStatus():
   output = ""
@@ -66,23 +95,16 @@ def playersStatus():
   elif(len(players) == playercount):
     output += printRolelist(players)
   else:
-    output += ("need " + (len(rolelist) - len(players)) + " more players \n")
+    output += ("need " + str(playercount - len(players)) + " more players \n")
   return output
-
-def setDefaultPlayers():
-  pl = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p" ]
-  players = pl[0:playercount]
-
-def addPlayer(player):
-  if(len(players) < playercount):
-    players.append(player)
 
 def printRolelist(players):
   output = ""
+  clean()
   output += ("------------Roles-------------\n") 
   c = 0 
-  initializeRolelist(playercount)
-  roles = assignRoles(rolelist)
+  global roles 
+  roles = setRoles(rolelist)
   random.shuffle(players)
   for entry in rolelist:
     output += (entry + " - " + players[c]+ " (" +roles[c]+")\n") 
