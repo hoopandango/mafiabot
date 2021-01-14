f8b1bd25bfe75d4a6bd6a79030f7262e98bad297
hoopandango <76955186+hoopandango@users.noreply.github.com>
2021-01-14 11:16:52 +0530

    Update roleManagement.py
    added rigging, can now sort rigged lists.

--- roleManagement.py
+++ roleManagement.py
@@ -6,6 +6,7 @@ roles = []  # List of roles in current game
 allRolelists = {} # Collection of all rolelists
 rolelist = [] # Rolelist used in current game
 allRoles = [] # List of all roles [each role is a dictionary]
+possibleRoles = [] # List of roles that are free
 playercount = 10 # Number of players
 
 def clean():
@@ -55,8 +56,11 @@ def setPlayercount(num):
   playercount = num
 
 def getPlayers():
+  players2 = players.copy()
+  random.shuffle(players2)
+
   output = "------------Players-------------\n"
-  for player in players:
+  for player in players2:
     output += (player+"\n")
   output += "\n"
   return output
@@ -64,6 +68,7 @@ def getPlayers():
 def addPlayer(playerlist):
   global players
   playerarray = playerlist.split(',')
+  playerarray = [x.strip() for x in playerarray] 
   if(len(players)+len(playerarray) <= playercount):
     players += playerarray
   return str(len(playerarray))
@@ -75,35 +80,37 @@ def setDefaultPlayers():
 
 def setRoles(rolelist): #sets roles to roles[]
   rolechoices= []  #roles that can be set in a slot
+  global possibleRoles
   global roles
-  global allRoles
+  possibleRoles = allRoles.copy()
   #if(playercount <= 11):
   #    allRoles.remove([t for t in allRoles if t["name"] =="Jailor"])
   #    allRoles.remove([t for t in allRoles if t["name"] =="Wolf Apprentice"])
   for e in rolelist:
     if e == "Jailor" or e == "Alphawolf" or e == "Wolf Apprentice":
-      rolechoices = [t for t in allRoles if t["name"] == e] 
-    elif e == "TI":
-      rolechoices = findRolechoices("T","I") 
-    elif e == "TP":
-      rolechoices = findRolechoices("T","P")   
+      rolechoices = [t for t in possibleRoles if t["name"] == e] 
     elif e == "TK":
-      rolechoices = findRolechoices("T","K") 
+      rolechoices = findRolechoices(possibleRoles,"T","K") 
+    elif e == "TI":
+      rolechoices = findRolechoices(possibleRoles,"T","I") 
+    elif e == "TP":
+      rolechoices = findRolechoices(possibleRoles,"T","P")    
     elif e == "TS":
-      rolechoices = findRolechoices("T","S")   
+      rolechoices = findRolechoices(possibleRoles,"T","S")   
     else:
-      rolechoices = findRolechoices(e[-1]) 
+      rolechoices = findRolechoices(possibleRoles,e[-1]) 
     chosenrole = random.choice(rolechoices) 
-    roles.append(chosenrole["name"]) 
+    roles.append(chosenrole) 
     if(chosenrole["unique"]):
-      allRoles.remove(chosenrole) 
+      possibleRoles.remove(chosenrole) 
   return roles
 
-def findRolechoices(faction, *args):
+def findRolechoices(roleset,faction, *args):
   if(len(args)>0):
-    return [t for t in allRoles if t["roletype"] == args[0] and t["faction"] == faction]
+    return [t for t in roleset if t["roletype"] == args[0] and t["faction"] == faction]
   else:
-    return [t for t in allRoles if t["faction"] == faction]
+    return [t for t in roleset if t["faction"] == faction]
+
 
 def playersStatus():
   output = ""
@@ -128,7 +135,47 @@ def printRolelist():
   output += ("------------Roles-------------\n") 
   c = 0 
   for entry in rolelist:
-    output += (str(c) + " " + entry + " - " + players[c]+ " (" +roles[c]+")\n") 
+    output += (str(c) + ") " + entry + " - " + players[c]+ " (" +roles[c]["name"]+")\n") 
     c+=1 
   output += "\n"
   return output
+
+def rig(n,r):
+  global roles 
+  role = next((x for x in allRoles if x["name"] == r), "False")
+  if role == "False":
+    return "Role not found"
+  else:
+    roles[n] = role
+    return "Rigged"
+
+def swap(n1,n2):
+  global players
+  players[n1], players[n2] = players[n2], players[n1]
+  return "Swapped"
+
+def sort():
+  global roles
+  global players
+  unlockedRoles = []
+  n1 = 0
+  n2 = 0
+  for e in rolelist:
+    unlockedRoles = roles[n1:]
+    if e == "Jailor" or e == "Alphawolf" or e == "Wolf Apprentice":
+      rolechoices = [t for t in unlockedRoles if t["name"] == e]
+    elif e == "TK":
+      rolechoices = findRolechoices(unlockedRoles,"T","K") 
+    elif e == "TI":
+      rolechoices = findRolechoices(unlockedRoles,"T","I") 
+    elif e == "TP":
+      rolechoices = findRolechoices(unlockedRoles,"T","P")    
+    elif e == "TS":
+      rolechoices = findRolechoices(unlockedRoles,"T","S")   
+    else:
+      rolechoices = findRolechoices(unlockedRoles,e[-1]) 
+    n2 = roles.index(rolechoices[0])
+    players[n1], players[n2] = players[n2], players[n1]
+    roles[n1], roles[n2] = roles[n2], roles[n1]
+    n1+=1
+  return "Sorted"
